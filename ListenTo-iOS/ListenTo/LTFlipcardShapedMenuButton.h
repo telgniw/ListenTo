a43a71722089574ad914608dcd80d379b36bc01d
telgniw <b97902044@ntu.edu.tw>
2013-05-16 16:49:08 +0800

    Update storyboard.

---
+++ ListenTo-iOS/ListenTo/LTFlipcardShapedMenuButton.h
@@ -0,0 +1,13 @@
+//
+//  LTFlipcardShapedMenuButton.h
+//  ListenTo
+//
+//  Created by Yi Huang on 13/5/16.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import "OBShapedButton.h"
+
+@interface LTFlipcardShapedMenuButton : OBShapedButton
+
+@end
---
+++ ListenTo-iOS/ListenTo/LTFlipcardShapedMenuButton.m
@@ -0,0 +1,31 @@
+//
+//  LTFlipcardShapedMenuButton.m
+//  ListenTo
+//
+//  Created by Yi Huang on 13/5/16.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import "LTFlipcardShapedMenuButton.h"
+
+@implementation LTFlipcardShapedMenuButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
