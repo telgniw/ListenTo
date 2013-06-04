83f70cf7b85c2b372ff884ea12f6245adca7b9ed
Johnny <johnny78716@hotmail.com>
2013-06-04 08:39:38 -0400

    add word car view

---
+++ ListenTo-iOS/ListenTo/LTWordCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  LTWordCardViewController.h
+//  ListenTo
+//
+//  Created by Johnny Bee on 13/6/3.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTWordCardViewController : UIViewController
+
+@end
---
+++ ListenTo-iOS/ListenTo/LTWordCardViewController.m
@@ -0,0 +1,38 @@
+//
+//  LTWordCardViewController.m
+//  ListenTo
+//
+//  Created by Johnny Bee on 13/6/3.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import "LTWordCardViewController.h"
+
+@interface LTWordCardViewController ()
+
+@end
+
+@implementation LTWordCardViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
