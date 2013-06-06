06e1fabbda7242440fcd3508182a2a700ae24a5c
telgniw <b97902044@ntu.edu.tw>
2013-06-06 11:07:06 +0800

    Hide navigation bar by default.

--- ListenTo-iOS/ListenTo/LTNavigationController.m
+++ ListenTo-iOS/ListenTo/LTNavigationController.m
@@ -16,11 +16,9 @@
     [super viewDidLoad];
 
     // Set the background of navigation bar transparent.
-//    UIImage *barImage = [UIImage imageNamed:@"bar.png"];
-//    [self.navigationBar setTranslucent:YES];
-//    [self.navigationBar setBackgroundImage:[LTUtility transparentImage] forBarMetrics:UIBarMetricsDefault];
-//    [self.navigationBar setBackgroundImage:barImage forBarMetrics:UIBarMetricsDefault];
-//    [self.navigationBar setShadowImage:[LTUtility emptyImage]];
+    [self.navigationBar setTranslucent:YES];
+    [self.navigationBar setBackgroundImage:[LTUtility transparentImage] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationBar setShadowImage:[LTUtility emptyImage]];
 }
 
 @end
