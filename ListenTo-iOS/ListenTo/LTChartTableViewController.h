d551500994e380c2df64fe2efce39f633bae074a
telgniw <b97902044@ntu.edu.tw>
2013-06-12 11:34:14 +0800

    Rescue some files back to fix the compile error problem.

---
+++ ListenTo-iOS/ListenTo/LTChartTableViewController.h
@@ -0,0 +1,42 @@
+//
+//  LTChartTableViewController.h
+//  ListenTo
+//
+//  Created by Johnny Bee on 13/6/11.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MIMColor.h"
+#import "MIMWallGraph.h"
+#import "DataManager.h"
+
+@interface LTChartTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate, WallGraphDelegate>
+{
+    
+    MIMWallGraph *mWallGraph;
+    
+    NSMutableArray *dataArrayFromCSV;
+    NSMutableArray *xDataArrayFromCSV;
+    
+    
+    NSArray *yValuesArray;
+    NSArray *xValuesArray;
+    NSArray *xTitlesArray;
+    NSArray *wallPropertiesArray;
+    
+    NSDictionary *xProperty;
+    NSDictionary *yProperty;
+    
+    NSDictionary *horizontalLinesProperties;
+    NSDictionary *verticalLinesProperties;
+    
+    
+    DataManager *dataManager_;
+
+}
+
+@property (strong, nonatomic) NSNumber *cid;
+
+
+@end
