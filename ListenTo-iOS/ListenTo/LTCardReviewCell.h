a5eb2bd9e6dc615c0bde9c5c34743d27605da590
Johnny <johnny78716@hotmail.com>
2013-06-12 20:06:55 -0400

    review cards added

---
+++ ListenTo-iOS/ListenTo/LTCardReviewCell.h
@@ -0,0 +1,13 @@
+//
+//  LTCardReviewCell.h
+//  ListenTo
+//
+//  Created by Johnny Bee on 13/6/12.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTCardReviewCell : UITableViewCell
+
+@end
---
+++ ListenTo-iOS/ListenTo/LTCardReviewCell.m
@@ -0,0 +1,29 @@
+//
+//  LTCardReviewCell.m
+//  ListenTo
+//
+//  Created by Johnny Bee on 13/6/12.
+//  Copyright (c) 2013年 Rabbit Wears Pants. All rights reserved.
+//
+
+#import "LTCardReviewCell.h"
+
+@implementation LTCardReviewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
